f044660f80d1125353342c7c0ff11999a0ada01d
Dorothy John <dorothyy.john@gmail.com>
2020-04-10 19:13:23 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,15 +1,21 @@
-# Write your code here. If you get stuck, at the very bottom of this lab we've included
-# the answers. You should ONLY check those answers if you get totally stuck. The important
-# thing is NOT to "complete labs," the important thing is to learn the skills you need
-# in order to complete the rest of this course and be ready for the next course!
+# Define a method that takes 
+#1 parameter & prints a string 
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
 
+#Define a method that takes 
+#2 parameter & prints a string 
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
-
-
-
-
-
-
+#Define a method that takes 
+#2 parameter - 1 is optional
+#It prints a string 
+def introduction_with_language_optional(name,language="Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
